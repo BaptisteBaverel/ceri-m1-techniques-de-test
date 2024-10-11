f2b3b1273ae75ead7c0ef44bc7f524bf5d484977
uapv2500706 <uapv2500706@e-mc130-08.ad.univ-avignon.fr>
2024-10-11 17:26:39 +0200

    test de run des tests mvn

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -0,0 +1,13 @@
+package fr.univavignon.pokedex.api;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class IPokedexFactoryTest {
+	@Test
+	public void primeFactorTest()
+	{
+		assertEquals("test", "test");
+	}
+}
