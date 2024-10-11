b061014477add119b860c78a1f06f0039064649b
uapv2500706 <uapv2500706@e-mc130-08.ad.univ-avignon.fr>
2024-10-11 18:57:22 +0200

    avancement tp3

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -1,12 +1,27 @@
 package fr.univavignon.pokedex.api;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
+import org.mockito.Mockito;
+
 public class IPokedexFactoryTest {
+	
+	@Before
+	public void initTestEnvironment() {
+		IPokedexFactory mockedPokedexFactory = Mockito.mock(IPokedexFactory.class);
+		// Mockito.when(mockedPokedexFactory.createPokedex()).thenReturn("monlogin");
+	}
+	@After
+	public void destroyTestEnvironment() {
+	// cette méthode est exécutée après chaque test
+	}
+	
 	@Test
-	public void primeFactorTest()
+	public void primeFactor2Test()
 	{
 		assertEquals("test", "test");
 	}
--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -1,5 +1,32 @@
 package fr.univavignon.pokedex.api;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
 public class IPokemonMetadataProviderTest {
 
+	private PokemonMetadata defaultPokemon;
+
+	@Before
+	public void initTestEnvironment() {
+		//IPokedexFactory mockedPokedexFactory = Mockito.mock(IPokedexFactory.class);
+		//PokemonMetadata pokemonMetadata = new PokemonMetadata(0, null, 0, 0, 0);
+		//Mockito.when(mockedPokedexFactory.getPokemonMetadata(1)).thenReturn(pokemonMetadata);
+		
+		//defaultPokemon = pokemonMetadata;
+	}
+	@After
+	public void destroyTestEnvironment() {
+		
+	}
+	
+	@Test
+	public void primeFactorTest()
+	{
+		assertEquals("test", "test");
+	}
+	
 }
