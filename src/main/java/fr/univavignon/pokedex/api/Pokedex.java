882324e89978ca0513e538271e005ddda02f76c2
baptiste.baverel <baptiste.baverel4@gmail.com>
2024-11-15 10:14:14 +0100

    Implementation pokemon factory

---
+++ src/main/java/fr/univavignon/pokedex/api/Pokedex.java
@@ -0,0 +1,89 @@
+package fr.univavignon.pokedex.api;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * IPokedex interface. An IPokedex aims to store all information about
+ * captured pokemon, as their default metadata as well.
+ * 
+ * @author fv
+ */
+public class Pokedex implements IPokedex {
+	private final PokemonMetadataProvider pokemonMetadataProvider = new PokemonMetadataProvider();
+	private final PokemonFactory pokemonFactory = new PokemonFactory();
+
+	public PokemonMetadata getPokemonMetadata(int index) throws PokedexException {
+		return pokemonMetadataProvider.getPokemonMetadata(index);
+	}
+
+	public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
+		return pokemonFactory.createPokemon(index, cp, hp, dust, candy);
+	}
+
+	List<Pokemon> pokemonList;
+	/**
+	 * Returns the number of pokemon this pokedex contains.
+	 * 
+	 * @return Number of pokemon in this pokedex.
+	 */
+	public int size() {
+		return pokemonList.size();
+	}
+	
+	/**
+	 * Adds the given <tt>pokemon</tt> to this pokedex and returns
+	 * it unique index.
+	 * 
+	 * @param pokemon Pokemon to add to this pokedex.
+	 * @return Index of this pokemon relative to this pokedex.
+	 */
+	public int addPokemon(Pokemon pokemon) {
+		pokemonList.add(pokemon);
+		return pokemonList.indexOf(pokemon);
+	}
+	
+	/**
+	 * Locates the pokemon identified by the given <tt>id</tt>.
+	 * 
+	 * @param id Unique pokedex relative identifier.
+	 * @return Pokemon denoted by the given identifier.
+	 * @throws PokedexException If the given <tt>index</tt> is not valid.
+	 */
+	public Pokemon getPokemon(int id) throws PokedexException {
+		if (id < 0 || id > 150) {
+			throw new PokedexException("Invalid Id");
+		}
+		for (Pokemon poke: pokemonList) {
+			if (poke.getIndex() == id) {
+				return poke;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Returns an unmodifiable list of all pokemons this pokedex contains.
+	 * 
+	 * @return Unmodifiable list of all pokemons.
+	 */
+	public List<Pokemon> getPokemons() {
+		return Collections.unmodifiableList(pokemonList);
+	}
+
+	/**
+	 * Returns an unmodifiable list of all pokemons this pokedex contains.
+	 * The list view will be sorted using the given <tt>order</tt>.
+	 * 
+	 * @param order Comparator instance used for sorting the created view.
+	 * @return Sorted unmodifiable list of all pokemons.
+	 */
+	public List<Pokemon> getPokemons(Comparator<Pokemon> order) {
+		List<Pokemon> cloned = new ArrayList<Pokemon>(pokemonList);
+		cloned.sort(order);
+		return Collections.unmodifiableList(cloned);
+	}
+	
+}
--- src/test/java/fr/univavignon/pokedex/api/PokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/PokedexTest.java
@@ -2,29 +2,30 @@ package fr.univavignon.pokedex.api;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-public class IPokedexTest {
+public class PokedexTest {
 	private Pokemon bulbizarre,	aquali;
-	private IPokedex mockedPokedex;
+	private Pokedex pokedex, pokedexFormated;
 
 	private Comparator<Pokemon> pokemonComparator;
 	private List<Pokemon> defaultListPokemon;
 	private List<Pokemon> sortedListPokemon;
 
 	@Before
-	public void initTestEnvironment() throws PokedexException {
+	public void initTestEnvironment() {
+		pokedex = new Pokedex();
 		bulbizarre = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56);
 		aquali = 	 new Pokemon(133, "Aquali", 186, 168, 260, 2729, 202, 5000, 4, 100);
-		PokedexException exception = new PokedexException("Invalid Id");
+
+		pokedexFormated = new Pokedex();
+		pokedexFormated.addPokemon(aquali);
+		pokedexFormated.addPokemon(bulbizarre);
 
 		defaultListPokemon = new ArrayList<Pokemon>();
 		defaultListPokemon.add(aquali);
@@ -34,46 +35,34 @@ public class IPokedexTest {
 		sortedListPokemon.add(bulbizarre);
 		sortedListPokemon.add(aquali);
 
-		mockedPokedex = Mockito.mock(IPokedex.class);
-		Mockito.when(mockedPokedex.size()).thenReturn(2);
-		Mockito.when(mockedPokedex.addPokemon(bulbizarre)).thenReturn(1);
-		Mockito.when(mockedPokedex.addPokemon(aquali)).thenReturn(2);
-
-		Mockito.when(mockedPokedex.getPokemon(2)).thenReturn(aquali);
-		Mockito.when(mockedPokedex.getPokemon(-1)).thenThrow(exception);
-		Mockito.when(mockedPokedex.getPokemon(155)).thenThrow(exception);
-
-		Mockito.when(mockedPokedex.getPokemons()).thenReturn(defaultListPokemon);
-
 		pokemonComparator = new Comparator<Pokemon>() {
 			public int compare(Pokemon o1, Pokemon o2) {
 				return o1.getIndex() - o2.getIndex();
 			}
 		};
-		Mockito.when(mockedPokedex.getPokemons(pokemonComparator)).thenReturn(sortedListPokemon);
 	}
 
 	@Test
 	public void getSizeTest() {
-		int size = mockedPokedex.size();
-		assertTrue(size >=0 && size < 151 );
+		int size = pokedex.size();
+		assertTrue(size >= 0 && size < 151 );
 	}
 
 	@Test
 	public void addPokemonTest() {
-		assertEquals(2, mockedPokedex.addPokemon(aquali));
-		assertEquals(1, mockedPokedex.addPokemon(bulbizarre));
+		assertEquals(1, pokedex.addPokemon(aquali));
+		assertEquals(2, pokedex.addPokemon(bulbizarre));
 	}
 
 	@Test
 	public void getPokemonTest() throws PokedexException {
-		assertEquals(aquali, mockedPokedex.getPokemon(2));
+		assertEquals(aquali, pokedexFormated.getPokemon(133));
 
 		PokedexException exception1 = assertThrows(PokedexException.class, () -> {
-			mockedPokedex.getPokemon(-1);
+			pokedex.getPokemon(-1);
 		});
 		PokedexException exception2 = assertThrows(PokedexException.class, () -> {
-			mockedPokedex.getPokemon(155);
+			pokedex.getPokemon(155);
 		});
 
 		assertEquals(exception1.getMessage(), "Invalid Id");
@@ -82,11 +71,11 @@ public class IPokedexTest {
 
 	@Test
 	public void getPokemonsTest() {
-		assertEquals(defaultListPokemon, mockedPokedex.getPokemons());
+		assertEquals(defaultListPokemon, pokedex.getPokemons());
 	}
 
 	@Test
 	public void getPokemonsOrderedTest() {
-		assertEquals(sortedListPokemon, mockedPokedex.getPokemons(pokemonComparator));
+		assertEquals(sortedListPokemon, pokedex.getPokemons(pokemonComparator));
 	}
 }
