34e576a9b7f7909bb0ff7ea771c729ec47a88684
baptiste.baverel <baptiste.baverel4@gmail.com>
2024-11-15 10:15:13 +0100

    Implementation pokedex

--- src/test/java/fr/univavignon/pokedex/api/PokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/PokedexTest.java
@@ -23,6 +23,8 @@ public class PokedexTest {
 		bulbizarre = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56);
 		aquali = 	 new Pokemon(133, "Aquali", 186, 168, 260, 2729, 202, 5000, 4, 100);
 
+		pokedex = new Pokedex();
+
 		pokedexFormated = new Pokedex();
 		pokedexFormated.addPokemon(aquali);
 		pokedexFormated.addPokemon(bulbizarre);
